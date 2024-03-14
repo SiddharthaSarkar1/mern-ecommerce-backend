5cd5e5b3a615f33856ab0deb020c61667f77ecaa
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-15 01:15:56 +0530

    basic setup and create models

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require("express");
 const server = express();
+const mongoose = require("mongoose");
+const { createProduct } = require("./controller/Product");
+require("dotenv").config();
 
 const PORT = 5050;
+
+//Middlewares
+
+server.use(express.json()); //to parse req.body
+
+main();
+
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "sidd_ecommerce",
+    });
+    console.log("database connected");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+server.get("/", (req, res) => {
+  res.json({ status: "Success" });
+});
+
+server.post("/products", createProduct);
 
 server.listen(PORT, () => {
   `Server is running on port: ${PORT}`;
