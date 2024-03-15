8235acce6e71327ad9b64b73904fd7db77a3e72b
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-16 02:01:58 +0530

    product related api's

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const server = express();
 const mongoose = require("mongoose");
 const { createProduct } = require("./controller/Product");
 require("dotenv").config();
+const cors = require("cors");
+
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categoreis");
+const brandsRouter = require("./routes/Brands");
 
 const PORT = 5050;
 
 //Middlewares
-
+server.use(cors());
 server.use(express.json()); //to parse req.body
+server.use('/products', productsRouter.router);
+server.use('/categories', categoriesRouter.router);
+server.use('/brands', brandsRouter.router);
 
+// MongoDB connection
 main();
 
 async function main() {
