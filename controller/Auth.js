397bf8422d9703c85586c870624204c811d5d355
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-19 02:17:51 +0530

    started with passport

--- controller/Auth.js
+++ controller/Auth.js
@@ -11,22 +11,9 @@ exports.createUser = async (req, res) => {
 };
 
 exports.loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email }).exec();
-    // TODO: this is just temporary, we will use strong password auth
-    console.log({ user });
-    if (!user) {
-      res.status(401).json({ message: "no such user email" });
-    } else if (user.password === req.body.password) {
-      // TODO: We will make addresses independent of login
-      res.status(200).json({
-        id: user.id,
-        role: user.role
-      });
-    } else {
-      res.status(401).json({ message: "invalid credentials" });
-    }
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  res.json(req.user);
+};
+
+exports.checkUser = async (req, res) => {
+  res.json({status:'success',user: req.user});
 };
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const { createProduct } = require("./controller/Product");
 require("dotenv").config();
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categoreis");
@@ -12,10 +15,19 @@ const usersRouter = require("./routes/Users");
 const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const ordersRouter = require("./routes/Order");
+const { User } = require("./model/User");
 
 const PORT = 5050;
 
 //Middlewares
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate("session"));
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
@@ -29,6 +41,45 @@ server.use("/users", usersRouter.router);
 server.use("/auth", authRouter.router);
 server.use("/cart", cartRouter.router);
 server.use("/orders", ordersRouter.router);
+
+// Passport Strategies
+passport.use(
+  "local",
+  new LocalStrategy(async function (username, password, done) {
+    // by default passport uses username
+    try {
+      const user = await User.findOne({ email: username }).exec();
+      console.log(username, password, user);
+      console.log(user.password, password);
+      if (!user) {
+        return done(null, false, { message: "invalid credentials" }); // for safety
+      } else if (user.password === password) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "invalid credentials" });
+      }
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+  console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 // MongoDB connection
 main();
