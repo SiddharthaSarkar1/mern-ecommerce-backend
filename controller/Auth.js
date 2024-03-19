5eecd2050f60aac807646884a4dfb2fd7171b94f
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-20 01:06:49 +0530

    Passport implemented with local / JWT

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,10 +1,34 @@
 const { User } = require("../model/User");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const { sanitizeUser } = require("../services/common");
+
+const SECRET_KEY = "SECRET_KEY";
 
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
   try {
-    const doc = await user.save();
-    res.status(201).json({ id: doc.id, role: doc.role });
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(
+      req.body.password,
+      salt,
+      310000,
+      32,
+      "sha256",
+      async function (err, hashedPassword) {
+        const user = new User({ ...req.body, password: hashedPassword, salt });
+        const doc = await user.save();
+
+        req.login(sanitizeUser(doc), (err) => {
+          if (err) {
+            res.status(400).json(err);
+          } else {
+            const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+            res.status(201).json(token);
+          }
+        });
+        // res.status(201).json();
+      }
+    );
   } catch (err) {
     res.status(400).json(err);
   }
@@ -15,5 +39,5 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.checkUser = async (req, res) => {
-  res.json({status:'success',user: req.user});
+  res.json({ status: "success", user: req.user });
 };
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categoreis");
@@ -16,8 +20,16 @@ const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const ordersRouter = require("./routes/Order");
 const { User } = require("./model/User");
+const { isAuth, sanitizeUser } = require("./services/common");
 
 const PORT = 5050;
+
+const SECRET_KEY = "SECRET_KEY";
+// JWT options
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 
 //Middlewares
 server.use(
@@ -34,32 +46,63 @@ server.use(
   })
 );
 server.use(express.json()); //to parse req.body
-server.use("/products", productsRouter.router);
-server.use("/categories", categoriesRouter.router);
-server.use("/brands", brandsRouter.router);
-server.use("/users", usersRouter.router);
+server.use("/products", isAuth(), productsRouter.router);
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/users", isAuth(), usersRouter.router);
 server.use("/auth", authRouter.router);
-server.use("/cart", cartRouter.router);
-server.use("/orders", ordersRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/orders", isAuth(), ordersRouter.router);
 
 // Passport Strategies
 passport.use(
   "local",
-  new LocalStrategy(async function (username, password, done) {
+  new LocalStrategy(
+    {usernameField: 'email'},
+    async function (email, password, done) {
     // by default passport uses username
     try {
-      const user = await User.findOne({ email: username }).exec();
-      console.log(username, password, user);
-      console.log(user.password, password);
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
       if (!user) {
         return done(null, false, { message: "invalid credentials" }); // for safety
-      } else if (user.password === password) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: "invalid credentials" });
       }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: "invalid credentials" });
+          } else {
+            const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+            return done(null, token);
+          }
+        }
+      );
     } catch (err) {
       done(err);
+    }
+  })
+);
+
+// passport jwt strategy
+
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    console.log({ jwt_payload });
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user)); // this calls serializer
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
     }
   })
 );
